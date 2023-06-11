cb41a8e3a1301f7a86d03b8fadc0d5d795f3eeac
aznszn <akadri.bscs20seecs@student.nust.edu.pk>
2023-06-12 04:01:57 +0500

    corrections

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     X = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])
     Y = np.array([0, 1, 1, 0])  # output as row vector
 
-    lr = 3  #learning rate
+    lr = 1 #learning rate
     epochs = 1000
 
     n_x = 2 #features in input
@@ -15,32 +15,33 @@ if __name__ == '__main__':
     neurons_l1 = 2  #neurons in first layer
     neurons_l2 = 1  #neurons in output layer
 
-    W1 = np.random.random((n_x, neurons_l1))
+    W1 = np.random.random((neurons_l1, n_x))
     B1 = np.zeros((neurons_l1, 1))
 
-    W2 = np.random.random((neurons_l1, neurons_l2))
+    W2 = np.random.random((neurons_l2, neurons_l1))
     B2 = np.zeros((neurons_l2, 1))
 
     for i in range(epochs):
         #forward pass
-        Z1 = np.dot(W1.T, X) + B1
+        Z1 = np.dot(W1, X) + B1
         A1 = sigmoid(Z1)
 
-        Z2 = np.dot(W2.T, A1) + B2
+        Z2 = np.dot(W2, A1) + B2
         A2 = sigmoid(Z2)
 
         #backward pass
         DZ2 = A2 - Y        #using cross-entropy loss
-        DW2 = (1/m)*np.dot(A1, DZ2.T)
-        DB2 = (1/m)*np.sum(DZ2)
+        DW2 = (1/m)*np.dot(DZ2, A1.T)
+        DB2 = (1/m)*np.sum(DZ2, axis=1, keepdims=True)
 
         W2 = W2 - lr*DW2
         B2 = B2 - lr*DB2
 
         # backward pass for the first layer
-        DZ1 = np.dot(W2, DZ2) * A1 * (1 - A1)
-        DW1 = (1/m) * np.dot(X, DZ1.T)
-        DB1 = (1/m) * np.sum(DZ1)
+        DZ1 = np.dot(W2.T, DZ2) * A1 * (1 - A1)
+        print(DZ1)
+        DW1 = (1/m) * np.dot(DZ1, X.T)
+        DB1 = (1/m) * np.sum(DZ1, axis=1, keepdims=True)
 
         W1 = W1 - lr*DW1
         B1 = B1 - lr*DB1
@@ -50,4 +51,4 @@ if __name__ == '__main__':
             print('epoch = ', i)
             print('loss = ', logloss)
 
-print('\nXOR neural network final predictions after rounding:\n', np.concatenate((X, np.round(A2))).T)
+print('\nXOR neural network final predictions:\n', np.concatenate((X, A2)).T)
